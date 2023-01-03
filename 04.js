f81117f0fd9bd985dab2d29bd1a992d04c28fe79
Arthals <zhuozhiyongde@126.com>
2023-01-03 23:53:26 +0800

    i hate my lack of self-discipline

--- 04.js
+++ 04.js
@@ -1,6 +1,6 @@
 // Interesting compare
 
-let a = {
+/* let a = {
     value: 1,
 };
 
@@ -10,3 +10,26 @@ a[Symbol.toPrimitive] = function (hint) {
 };
 
 console.log(a == 1 && a == 2 && a == 3);
+ */
+
+// WeakMap
+let visitCountMap = new WeakMap();
+
+function counterUser(user) {
+    let count = visitCountMap.get(user) || 0;
+    visitCountMap.set(user, count + 1);
+}
+
+let john = { name: 'John' };
+counterUser(john);
+
+console.log(visitCountMap.get(john));
+
+john = null;
+
+console.log(visitCountMap.get(john));
+/* Output
+→ node 04.js
+1
+undefined
+ */
